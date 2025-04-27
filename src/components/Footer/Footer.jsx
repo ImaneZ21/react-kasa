cdfd4ba9488de3f082e63b48958f10497d5e8db8
ImaneZ21 <zerdag.imane@gmail.com>
2025-04-27 17:23:04 +0200

    footer css laptop + mobile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
+import image from "../../assets/logo_footer.png";
+import "./Footer.scss";
 
 function Footer() {
   return (
     <footer>
       <div className="footer">
-        
+        <img src={image} alt="logo footer"/>
+        <p>© 2020 Kasa. All<span>rights reserved</span></p>
       </div>
     </footer>
   );
